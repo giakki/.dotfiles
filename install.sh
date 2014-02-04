5a4e2de00fcf18649861c5a3067a1accf2a493e9
Giacomo Martino <giakki.dev@gmail.com>
2014-02-04 17:41:11 +0100

    zzz

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+pushd $(dirname $0)
+
+git submodule init
+git submodule update
+
 # Add custom theme
-mkdir ~/.dotfiles/oh-my-zsh/custom/themes/ 2> /dev/null
+mkdir ~/.dotfiles/oh-my-zsh/custom/themes/
 ln -s ~/.dotfiles/giakki.zsh-theme ~/.dotfiles/oh-my-zsh/custom/themes/giakki.zsh-theme
 # Link .zshrc
 rm ~/.zshrc 2> /dev/null
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
+echo "Installation successful!"
+
+popd
