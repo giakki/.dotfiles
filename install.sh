842a7b6741583e37572c344cdc90e30b05d9e3c0
Giacomo Martino <giakki.dev@gmail.com>
2014-02-04 17:24:36 +0100

    Custom theme

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-ln -s ~/.dotfiles/themes/giakki.zsh-theme ~/.dotfiles/oh-my-zsh/themes/giakki.zsh-theme
+# Add custom theme
+mkdir ~/.dotfiles/oh-my-zsh/custom/themes/
+ln -s ~/.dotfiles/themes/giakki.zsh-theme ~/.dotfiles/oh-my-zsh/custom/themes/giakki.zsh-theme
+# Link .zshrc
 ln -s ~/.zshrc ~/.dotfiles/.zshrc
